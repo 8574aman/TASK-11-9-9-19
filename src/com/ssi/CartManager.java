790b9a09ab8a1be104e5b4609dcb38c60793b82f
unknown <8574aman@gmail.com>
2019-09-12 11:24:40 +0530

    TASK-12-11-9-19..UPDATE PROFILE & SUBJECT FILTER SEARCH

--- src/com/ssi/CartManager.java
+++ src/com/ssi/CartManager.java
@@ -16,48 +16,47 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/CartManager")
 public class CartManager extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String bcode = request.getParameter("code");
-		HttpSession hs  = request.getSession();
-		String username = (String)hs.getAttribute("username");
-		HashMap<String,ArrayList<Integer>> map ;
+		HttpSession hs = request.getSession();
+		String username = (String) hs.getAttribute("username");
+		HashMap<String, ArrayList<Integer>> map;
 		ArrayList<Integer> bookCodes = new ArrayList<Integer>();
 		ServletContext sc1 = getServletContext();
-	 	 map = (HashMap<String,ArrayList<Integer>>)sc1.getAttribute("cart");
-		
-						 
-	if(map==null)
-	{
-		map = new HashMap<>();
-		bookCodes.add(Integer.parseInt(bcode));
-		map.put(username, bookCodes);
-		sc1.setAttribute("cart", map);
-	}
-	else
-	{
-		 
-		if(map.containsKey(username))
-		{
-			bookCodes = map.get(username);
+		map = (HashMap<String, ArrayList<Integer>>) sc1.getAttribute("cart");
+
+		if (map == null) {
+			
+			map = new HashMap<>();
 			bookCodes.add(Integer.parseInt(bcode));
 			map.put(username, bookCodes);
 			sc1.setAttribute("cart", map);
+	
 		}
 		else
 		{
-			bookCodes.add(Integer.parseInt(bcode));
-			map.put(username, bookCodes);
-			sc1.setAttribute("cart", map);
+
+			if (map.containsKey(username)) {
+				bookCodes = map.get(username);
+				bookCodes.add(Integer.parseInt(bcode));
+				map.put(username, bookCodes);
+				sc1.setAttribute("cart", map);
+			} else {
+				bookCodes.add(Integer.parseInt(bcode));
+				map.put(username, bookCodes);
+				sc1.setAttribute("cart", map);
+			}
+		
+		}
+
+		PrintWriter out = response.getWriter();
+		for (Integer x : bookCodes) 
+		{
+			out.print(x);
 		}
 	}
-	
-	PrintWriter out = response.getWriter();
-	for(Integer x:bookCodes)
-	{
-		out.print(x);
-	}		
-	}
 
-	
 }
